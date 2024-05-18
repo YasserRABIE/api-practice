4e48f1ac630642714acde021081f43aa9f209407
YasserRABIE <yasserrabie110306@gmail.com>
2024-05-18 15:10:10 +0300

    Add book CRUD functionality

--- main.go
+++ main.go
@@ -1,9 +1,67 @@
 package main
 
-import(
-  "net/https"
-  "github.com/gin-gonic/gin"
-  "errors"
+import (
+	"errors"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main(){}
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func bookById(c *gin.Context) {
+	book, err := getBookByid(c.Param("id"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookByid(id string) (*book, error) {
+	for idx, book := range books {
+		if book.ID == id {
+			return &books[idx], nil
+		}
+	}
+
+	return nil, errors.New("book not found")
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+
+	router.Run("localhost:8080")
+}
